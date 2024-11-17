6fb740a7e2db868c9c049fd9036dbc7530c4e573
Leonardo Henao <leonardofhenao.085@gmail.com>
2024-11-17 18:22:47 -0500

    Feat: Se permite subir al repositorio el archivo config.js eliminandolo del .gitignore

--- api-endpoints.js
+++ api-endpoints.js
@@ -1,8 +1,7 @@
 import { URL_API } from "./config.js";
 import { DefaultData } from "./default-data.js";
 
-const urlAPI =
-  document.location.hostname == "127.0.0.1" ? URL_API : process.env.URL_API;
+const urlAPI = URL_API;
 const headers = {
   "content-type": "application/json",
 };
---
+++ config.js
@@ -0,0 +1,1 @@
+export const URL_API = "https://67365809aafa2ef222304614.mockapi.io/api/v777";
